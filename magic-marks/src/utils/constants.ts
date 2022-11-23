3ac31d7e4521addeafe93eb6874e193404fa64bf
AnastasiyaPoleshuk <anastasiyapaliashuk@gmail.com>
2022-11-23 15:32:38 +0300

    MM-29: update mm app for tablet

--- magic-marks/src/utils/constants.ts
+++ magic-marks/src/utils/constants.ts
@@ -1,6 +1,6 @@
 const CONSTANTS = {
-  URL: 'https://magic-marks-api.herokuapp.com',
-  // URL: 'http://localhost:5000',
+  // URL: 'https://magic-marks-api.herokuapp.com',
+  URL: 'http://localhost:5000',
   LOGIN__MODAL: 'LOGIN__MODAL',
   MARKS__MODAL: 'MARKS__MODAL',
   MARKS_DELETE__MODAL: 'MARKS_DELETE__MODAL',
