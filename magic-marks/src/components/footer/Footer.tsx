08762220849428caef4206dd9991dc93f8a668e7
Anastasiya Poleshuk <anastasiyapaliashuk@gmail.com>
2024-08-28 10:20:28 +0300

    refactor styles

--- magic-marks/src/components/footer/Footer.tsx
+++ magic-marks/src/components/footer/Footer.tsx
@@ -1,10 +1,14 @@
+import dayjs from 'dayjs';
 import './Footer.scss';
 
 const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer__container container">
-        <div className="footer-item">&copy;2022</div>
+        <div className="footer-item">
+          &copy;
+          {dayjs().year()}
+        </div>
         <a href="https://github.com/AnastasiyaPoleshuk" className="footer-item footer-item__link">Anastasiya Poleshuk</a>
       </div>
     </footer>
